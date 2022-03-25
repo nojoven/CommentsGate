2b5e45a9864e3476718099178baab094fb857845
nojoven <work.cedricjoseph@gmail.com>
2022-03-25 19:59:05 +0100

    chore: small cleaning

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,24 +9,6 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-
-def test_select_language():
-   """Testing the French path"""
-   response = client.get("/lang/fr/publish")
-   assert response.status_code == 200
-   assert response.json() is not None
-   assert "Français" in  response.json()["message"]
-
-   """Testing the English path"""
-   response = client.get("/lang/en/publish")
-   assert response.status_code == 200
-   assert response.json() is not None
-   assert "English" in  response.json()["message"]
-
 def test_get_all_target_comments():
    targetId = TARGET_ID_EXAMPLE
 
@@ -43,7 +25,7 @@ def test_create_new_comment():
    comment["authorId"] = "cjo8922"
    comment["targetId"] = TARGET_ID_EXAMPLE
    comment["replies"] = ["aaaa"]
-   print(comment)
+
    response = client.post(
       f"/target/{TARGET_ID_EXAMPLE}/comments", 
       data=json.dumps(comment),

f1a618cdef2f2060a6a17658340764ba76988108
nojoven <work.cedricjoseph@gmail.com>
2022-03-25 21:43:48 +0100

    test: update tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -2,32 +2,27 @@ from datetime import datetime
 import json
 from fastapi.testclient import TestClient
 
-from utils.constants import TARGET_ID_EXAMPLE
+from utils.constants import TARGET_ID_EXAMPLE as targetId_test
 from app.main import app
-
 
 client = TestClient(app)
 
-
-def test_get_all_target_comments():
-   targetId = TARGET_ID_EXAMPLE
-
-   response = client.get(f"/target/{targetId}/comments")
-   assert response.status_code == 200
-   assert response.json() == True
-
 def test_create_new_comment():
    comment = dict()
-   comment["id"] = "opcrc-6575"
    comment["textFr"] = "Bien joué!"
    comment["textEn"] = "Well Done!"
    comment["publishedAt"] = str(datetime.now().timestamp())
-   comment["authorId"] = "cjo8922"
-   comment["targetId"] = TARGET_ID_EXAMPLE
-   comment["replies"] = ["aaaa"]
+   comment["authorId"] = "cjo2022"
+   comment["targetId"] = targetId_test
 
    response = client.post(
-      f"/target/{TARGET_ID_EXAMPLE}/comments", 
+      f"/target/{targetId_test}/comments", 
       data=json.dumps(comment),
       headers={"Content-Type": "application/json"})
-   assert response.status_code == 201
+   assert response.status_code == 200
+
+def test_get_all_target_comments():
+      response = client.get(f"/target/{targetId_test}/comments")
+      assert response.status_code == 200
+      assert isinstance(json.loads(response.content), list)
+      assert "replies" in json.loads(response.content)[0].keys()

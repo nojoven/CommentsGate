5b3a3369cf34eed07357e24e7fb9e66cb3def854
nojoven <work.cedricjoseph@gmail.com>
2022-03-25 20:22:58 +0100

    feat: add translation module to project, write helpers

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,9 +1,17 @@
 """This file contains helper functions."""
 import uuid
 
+from deep_translator import DeeplTranslator
+
 
 def generate_uuid():
     return str(uuid.uuid4())
+
+def is_french_text(text: str):
+   pass
+
+def is_english_text(text: str):
+   pass
 
 def is_timestamp(string: str):
    """
@@ -15,6 +23,13 @@ def is_timestamp(string: str):
    except ValueError:
       return False
 
+def translate_en_to_fr(text: str):
+   """Translates English text."""
+   return DeeplTranslator(source='en', target='fr').translate(text)
+   
+def translate_fr_to_en(text: str):
+   """Translates French text."""
+   return DeeplTranslator(source='fr', target='en').translate(text)
 
 def trigger_eager_load(data):
    """

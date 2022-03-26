519c6495bf56b6962f424306ec94ff956b035f9c
nojoven <work.cedricjoseph@gmail.com>
2022-03-26 21:23:06 +0100

    feat: add fastapi default port to settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,8 @@
 import os
+# DEPLOYMENT
+SERVICE_PORT = os.environ.get('SERVICE_PORT')
 
-# APIs 
+# External APIs 
 LANG_DETECTION_API_KEY  = os.environ.get('LANG_DETECTION_API_KEY')
 DEEPL_API_KEY = os.environ.get('DEEPL_API_KEY')
 
